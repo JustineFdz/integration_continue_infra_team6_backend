af332b93593fb3c15be10d96bb4e87768e09847a
mfrancois-5432 <daedalus5432@gmail.com>
2023-12-10 20:57:31 +0100

    finish

--- Backend/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ Backend/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,25 +1,39 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
 import prismadb from "@/lib/prismadb";
 
-interface DashboardPageProps {
-  params: {
-    storeId: string;
-  };
-};
+import { SettingsForm } from "@/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form";
 
-const DashboardPage: React.FC<DashboardPageProps> = async ({ 
+const SettingsPage = async ({
   params
+}: {
+  params: { storeId: string }
 }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
   const store = await prismadb.store.findFirst({
     where: {
-      id: params.storeId
+      id: params.storeId,
+      userId
     }
   });
 
-  return (
-    <div>
-      Active Store : {store?.name}
+  if (!store) {
+    redirect('/');
+  }
+
+  return ( 
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
     </div>
   );
-};
+}
 
-export default DashboardPage;
+export default SettingsPage;
